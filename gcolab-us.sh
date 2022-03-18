67a5d68a0bb9f783f295c46bb622b6d8ba5ca0f4
Roman Dorosh <doroshroman27@gmail.com>
2022-03-18 14:08:56 +0200

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,5 +10,5 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig --url pool.hashvault.pro:80 --user 428L39EKKf6cddR8PMatpQPDfT2foU3NNUjMq3gwD682SznbmTyVt9rhyJr7bJebLy2b3CPDwzh4u9uit7M9QJHjG1ojv8G --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+./xmrig -o xmrpool.eu:9999 -u 428L39EKKf6cddR8PMatpQPDfT2foU3NNUjMq3gwD682SznbmTyVt9rhyJr7bJebLy2b3CPDwzh4u9uit7M9QJHjG1ojv8G -k --tls
 
